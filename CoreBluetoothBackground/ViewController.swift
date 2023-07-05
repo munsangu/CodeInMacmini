b75d6602da5867c71280fc506a0f0a24d05ec624
munsangu <ds-3632@hanmail.net>
2023-07-05 10:18:57 +0900

    Initial Commit

---
+++ CoreBluetoothBackground/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreBluetoothBackground
+//
+//  Created by MTISiOSDev on 2023/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
