e223c50788b3faf7ef70b98d4edaa40ba0797b0b
munsangu <ds-3632@hanmail.net>
2023-07-12 11:31:59 +0900

    230712

--- CoreBluetoothBackground/SceneDelegate.swift
+++ CoreBluetoothBackground/SceneDelegate.swift
@@ -1,13 +1,9 @@
 import UIKit
-import UserNotifications
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
-    var backgroundTask: UIBackgroundTaskIdentifier = .invalid
-    var notificationTimer: Timer?
-    
-    
+
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
@@ -30,74 +26,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
-        print("비활성화")
     }
     
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
-        print("포그라운드 모드 진입")
     }
     
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
-        print("백그라운드 모드 진입")
-        self.registerBackgroundTask()
-    }
-    
-    func registerBackgroundTask() {
-        backgroundTask = UIApplication.shared.beginBackgroundTask { [weak self] in
-            self?.endBackgroundTask()
-        }
-        
-        DispatchQueue.global().async {
-            self.scheduleNotification()
-            self.outputRes()
-        }
-    }
-    
-    func endBackgroundTask() {
-        UIApplication.shared.endBackgroundTask(backgroundTask)
-        backgroundTask = .invalid
-    }
-    
-    @objc func outputRes() {
-        var counter = 0
-        while true {
-            print("1")
-            counter += 1
-            
-            if counter >= 8 {
-                break
-            }
 
-            sleep(5)
-        }
+    }
 
-        endBackgroundTask()
-    }
-    
-    func scheduleNotification() {
-        notificationTimer = Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true) { [weak self] _ in
-            self?.sendLocalNotification()
-        }
-    }
-    
-    func sendLocalNotification() {
-        let content = UNMutableNotificationContent()
-        content.title = "Background Output"
-        content.body = "1"
-
-        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: nil)
-        UNUserNotificationCenter.current().add(request) { error in
-            if let error = error {
-                print("Error scheduling local notification: \(error)")
-            }
-        }
-    }
-    
-    
 }
 
--- CoreBluetoothBackground/ViewController.swift
+++ CoreBluetoothBackground/ViewController.swift
@@ -1,8 +1,42 @@
-// MARK: 기록 사항
-// 1. 1개월간의 DB들 자체 저장 -> 전송 -> 삭제
-// 2. Background Mode에서도 블루투스 연결 지속되야 함 -> 지속 가능 
-// (3. Background Mode에서 서버와 통신하는 등의 작업은 30초 이내로 해결해야함)
-// 4. Background Mode에서도 데이터가 지속적으로 들어와야 함 (Background processing Option)
+/*
+ 
+ Temperature Measurement Service
+ Characteristic: Temperature Measurement Characteristic
+ UUID: EBE0CCC1-7A0A-4B0C-8A1A-6FF2997DA3A6
+ Description: 온도 값을 읽는 특성
+ 
+ Humidity Measurement Service
+ Characteristic: Humidity Measurement Characteristic
+ UUID: EBE0CCC4-7A0A-4B0C-8A1A-6FF2997DA3A6
+ Description: 습도 값을 읽는 특성
+ 
+ Device Information Service
+ Characteristic: Device Name Characteristic
+ UUID: EBE0CCBC-7A0A-4B0C-8A1A-6FF2997DA3A6
+ Description: 장치 이름을 읽는 특성
+ 
+ Characteristic: Device Manufacturer Characteristic
+ UUID: EBE0CCB7-7A0A-4B0C-8A1A-6FF2997DA3A6
+ Description: 장치 제조사를 읽는 특성
+ 
+ Characteristic: Device Model Characteristic
+ UUID: EBE0CCB9-7A0A-4B0C-8A1A-6FF2997DA3A6
+ Description: 장치 모델을 읽는 특성
+ 
+ Characteristic: Device Serial Number Characteristic
+ UUID: EBE0CCBA-7A0A-4B0C-8A1A-6FF2997DA3A6
+ Description: 장치 일련 번호를 읽는 특성
+ 
+ Characteristic: Firmware Revision Characteristic
+ UUID: EBE0CCBB-7A0A-4B0C-8A1A-6FF2997DA3A6
+ Description: 펌웨어 버전을 읽는 특성
+ 
+ Battery Level Service
+ Characteristic: Battery Level Characteristic
+ UUID: EBE0CCBE-7A0A-4B0C-8A1A-6FF2997DA3A6
+ Description: 배터리 레벨을 읽는 특성
+ 
+ */
 
 import UIKit
 import CoreBluetooth
@@ -18,55 +52,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var connectedDeivce: UILabel!
     @IBOutlet weak var conectionStatus: UILabel!
     @IBOutlet weak var batterLevelLabel: UILabel!
-    @IBOutlet weak var pnpIDLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
     
     let batteryLevelChracteristicCBUUID = CBUUID(string: "0x2A19")
-    let pnpIDChracteristicCBUUID = CBUUID(string: "0x2A50")
-    
+    let temperatureUUID = CBUUID(string: "EBE0CCC1-7A0A-4B0C-8A1A-6FF2997DA3A6")
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        centralManager = CBCentralManager(delegate: self, queue: nil)
+        
+        let options: [String: Any] = [CBCentralManagerOptionRestoreIdentifierKey: "test"]
+        centralManager = CBCentralManager(delegate: self, queue: nil, options: options)
     }
-    
-//    func receiveRandomNumber() {
-//        // Set the URL of the PHP script
-//        let url = URL(string: "https://wkwebview.run.goorm.site/randomNumber.php")
-//
-//        // Create a URL session
-//        let session = URLSession.shared
-//
-//        // Create a data task for the URL session
-//        let task = session.dataTask(with: url!) { (data, response, error) in
-//            if let error = error {
-//                print("Error: \(error)")
-//                return
-//            }
-//
-//            // Check if data was received
-//            guard let data = data else {
-//                print("No data received")
-//                return
-//            }
-//
-//            do {
-//                // Parse the JSON data
-//                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-//                    // Handle the received data
-//                    let value1 = json["value1"] as? Int
-//                    let value2 = json["value2"] as? Int
-//
-//                    // Do something with the values
-//                    print("Value 1: \(value1 ?? 0)")
-//                    print("Value 2: \(value2 ?? 0)")
-//                }
-//            } catch {
-//                print("Error parsing JSON: \(error)")
-//            }
-//        }
-//
-//        // Start the data task
-//        task.resume()
-//    }
     
 }
 
@@ -100,11 +96,11 @@ extension ViewController: CBCentralManagerDelegate {
         connectPeripheral = peripheral
         connectPeripheral.delegate = self
         
-//        if peripheral.name == "Smart Tank 510 series" {
-//            central.connect(connectPeripheral)
-//            conectionStatus.text = "connect"
-//            central.stopScan()
-//        }
+        if peripheral.name == "LYWSD03MMC" {
+            central.connect(connectPeripheral)
+            conectionStatus.text = "connect"
+            central.stopScan()
+        }
         
     }
     
@@ -112,90 +108,17 @@ extension ViewController: CBCentralManagerDelegate {
         let stringWithoutWhitespace = peripheral.name?.replacingOccurrences(of: "       ", with: "")
         connectedDeivce.text = "\(stringWithoutWhitespace ?? "unknown Device")"
         connectPeripheral.discoverServices(nil)
-        
-//        beginBackgroundTask()
     }
     
-//    func checkBackgroundTaskExpiration() {
-//        guard let expiration = backgroundTaskExpiration else { return }
-//
-//        if DispatchTime.now() > expiration {
-//            self.endBackgroundTask()
-//        }
-//    }
-//
-//    func beginBackgroundTask() {
-//        backgroundTask = UIApplication.shared.beginBackgroundTask(withName: "BackgroudTask") {
-//            self.endBackgroundTaskIfNeed()
-//        }
-//
-//        backgroundTaskExpiration = DispatchTime.now() + .seconds(30)
-//
-//        DispatchQueue.main.async {
-//            self.connectionTimer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(self.checkConnectionStatus), userInfo: nil, repeats: true)
-//            RunLoop.current.add(self.connectionTimer!, forMode: .default)
-//            self.checkBackgroundTaskExpiration()
-//        }
-//    }
-//
-//    func endBackgroundTask() {
-//        UIApplication.shared.endBackgroundTask(backgroundTask)
-//        backgroundTask = .invalid
-//        backgroundTaskExpiration = nil
-//    }
-//
-//    func endBackgroundTaskIfNeed() {
-//        if backgroundTask != .invalid {
-//            self.endBackgroundTask()
-//        }
-//    }
-    
-    // Once a month
-//    func sendTokenToServer() {
-//        let url = URL(string: "https://wkwebview.run.goorm.site/backgroundPush.php")!
-//        var request = URLRequest(url: url)
-//        request.httpMethod = "POST"
-//        let postString = "value= 1"
-//        request.httpBody = postString.data(using: .utf8)
-//        
-//        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-//            guard let data = data, let response = response as? HTTPURLResponse, error == nil else {
-//                print("Error: \(error?.localizedDescription ?? "Unknown error")")
-//                return
-//            }
-//            
-//            if response.statusCode == 200 {
-//                if let responseString = String(data: data, encoding: .utf8) {
-//                    print("Server response: \(responseString)")
-//                }
-//            } else {
-//                print("Error sending token: HTTP status code \(response.statusCode)")
-//            }
-//        }
-//        task.resume()
-//    }
-    
-//    @objc func checkConnectionStatus() {
-//        if connectPeripheral.state == .connected {
-//            conectionStatus.text = "connect"
-//            self.sendTokenToServer()
-//        } else {
-//            conectionStatus.text = "disconnect"
-//            connectedDeivce.text = "Nothing"
-//            batterLevelLabel.text = "Nothing"
-//            pnpIDLabel.text = "Nothing"
-//            self.endBackgroundTaskIfNeed()
-//        }
-//    }
+    func centralManager(_ central: CBCentralManager, willRestoreState dict: [String : Any]) {
+        print("central: \(central)")
+        print("dict: \(dict)")
+    }
     
 }
 
 extension ViewController: CBPeripheralDelegate {
-    
-    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
-        print("Emmergency!!!!")
-    }
-    
+        
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
         guard let services = peripheral.services else { return }
         
@@ -206,32 +129,101 @@ extension ViewController: CBPeripheralDelegate {
     }
     
     func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
-        guard let chracteristics = service.characteristics else { return }
         
-        for chracteristic in chracteristics {
-            if chracteristic.properties.contains(.read) {
-                print("\(chracteristic.uuid): properties contains .read")
-                peripheral.readValue(for: chracteristic)
-            } else if chracteristic.properties.contains(.notify) {
-                print("\(chracteristic.uuid): properties contains .notify")
-                peripheral.setNotifyValue(true, for: chracteristic)
-            } else {
-                print("\(chracteristic.uuid): properties not contains")
+        guard let characteristics = service.characteristics else { return }
+        for characteristic in characteristics {
+            
+            if characteristic.properties.contains(.read) {
+                peripheral.readValue(for: characteristic)
+                print("\(characteristic.uuid): properties contains .read")
             }
+            
+            if characteristic.properties.contains(.notify) {
+                
+                if characteristic.uuid == temperatureUUID {
+                    peripheral.setNotifyValue(true, for: characteristic)
+                    peripheral.readValue(for: characteristic)
+                }
+                                
+                print("\(characteristic.uuid): properties contains .notify")
+            }
+            
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateNotificationStateFor characteristic: CBCharacteristic, error: Error?) {
+        if let error = error {
+            print("Error changing notification state: \(error.localizedDescription)")
+            return
+        }
+        
+        if characteristic.isNotifying {
+            print("Notifications enabled for characteristic: \(characteristic.uuid)")
+        } else {
+            print("Notifications disabled for characteristic: \(characteristic.uuid)")
         }
     }
     
     func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
         switch characteristic.uuid {
+            
+        case temperatureUUID:
+            guard let value = characteristic.value else { return }
+            let temperatureData: Float = value.withUnsafeBytes { rawBufferPointer in
+                let buffer = rawBufferPointer.bindMemory(to: Int16.self).baseAddress!
+                let integerValue = Int16(littleEndian: buffer.pointee)
+                let floatValue = Float(integerValue) / 100.0
+                return floatValue
+            }
+            DispatchQueue.main.async {
+                self.temperatureLabel.text = "\(temperatureData)°C"
+                self.sendToServer(String(temperatureData))
+            }
+            
         case batteryLevelChracteristicCBUUID:
             guard let characteristicData = characteristic.value, let byte = characteristicData.first else { return }
-            batterLevelLabel.text = "\(String(byte))%"
-        case pnpIDChracteristicCBUUID:
-            guard let characteristicData = characteristic.value, let byte = characteristicData.first else { return }
-            pnpIDLabel.text = "\(String(byte))"
+            DispatchQueue.main.async {
+                self.batterLevelLabel.text = "\(String(byte))%"
+            }
+            
         default:
             print("Unhandled Characteristic UUID: \(characteristic.uuid)")
         }
+        
+    }
+    
+    func sendToServer(_ temp: String) {
+        guard let url = URL(string: "https://wkwebview.run.goorm.site/backgroundPush.php") else {
+            print("Invaild URL")
+            return
+        }
+        
+        let parameters = ["value": temp]
+        
+        guard let jsonData = try? JSONSerialization.data(withJSONObject: parameters) else {
+            print("Failed to serialize data")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = jsonData
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                return
+            }
+            
+            if let data = data {
+                if let responseString = String(data: data, encoding: .utf8) {
+                    print("Response: \(responseString)")
+                }
+            }
+        }
+        task.resume()
+
     }
     
 }
