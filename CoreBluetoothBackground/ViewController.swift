99df0cf42b07a662826dea664c016d4d28cce08a
munsangu <ds-3632@hanmail.net>
2023-07-18 15:08:44 +0900

    230718

--- CoreBluetoothBackground/ViewController.swift
+++ CoreBluetoothBackground/ViewController.swift
@@ -1,50 +1,14 @@
-/*
- 
- Temperature Measurement Service
- Characteristic: Temperature Measurement Characteristic
- UUID: EBE0CCC1-7A0A-4B0C-8A1A-6FF2997DA3A6
- Description: 온도 값을 읽는 특성
- 
- Humidity Measurement Service
- Characteristic: Humidity Measurement Characteristic
- UUID: EBE0CCC4-7A0A-4B0C-8A1A-6FF2997DA3A6
- Description: 습도 값을 읽는 특성
- 
- Device Information Service
- Characteristic: Device Name Characteristic
- UUID: EBE0CCBC-7A0A-4B0C-8A1A-6FF2997DA3A6
- Description: 장치 이름을 읽는 특성
- 
- Characteristic: Device Manufacturer Characteristic
- UUID: EBE0CCB7-7A0A-4B0C-8A1A-6FF2997DA3A6
- Description: 장치 제조사를 읽는 특성
- 
- Characteristic: Device Model Characteristic
- UUID: EBE0CCB9-7A0A-4B0C-8A1A-6FF2997DA3A6
- Description: 장치 모델을 읽는 특성
- 
- Characteristic: Device Serial Number Characteristic
- UUID: EBE0CCBA-7A0A-4B0C-8A1A-6FF2997DA3A6
- Description: 장치 일련 번호를 읽는 특성
- 
- Characteristic: Firmware Revision Characteristic
- UUID: EBE0CCBB-7A0A-4B0C-8A1A-6FF2997DA3A6
- Description: 펌웨어 버전을 읽는 특성
- 
- Battery Level Service
- Characteristic: Battery Level Characteristic
- UUID: EBE0CCBE-7A0A-4B0C-8A1A-6FF2997DA3A6
- Description: 배터리 레벨을 읽는 특성
- 
- */
-
 import UIKit
 import CoreBluetooth
+import AVFoundation
 
 class ViewController: UIViewController {
     
     var centralManager: CBCentralManager!
     var connectPeripheral: CBPeripheral!
+    
+    let audioSession = AVAudioSession.sharedInstance()
+    var audioPlayer: AVAudioPlayer?
     
     @IBOutlet weak var connectedDeivce: UILabel!
     @IBOutlet weak var conectionStatus: UILabel!
@@ -52,9 +16,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var humidityLabel: UILabel!
     
-    let batteryLevelChracteristicCBUUID = CBUUID(string: "0x2A19")
-    let temperatureUUID = CBUUID(string: "EBE0CCC1-7A0A-4B0C-8A1A-6FF2997DA3A6")
-    let humidityUUID = CBUUID(string: "EBE0CCBC-7A0A-4B0C-8A1A-6FF2997DA3A6")
+//    let batteryLevelChracteristicCBUUID = CBUUID(string: "0x2A19")
+    let temperatureAndHumidityUUID = CBUUID(string: "EBE0CCC1-7A0A-4B0C-8A1A-6FF2997DA3A6")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -108,20 +71,23 @@ extension ViewController: CBCentralManagerDelegate {
         connectPeripheral.discoverServices(nil)
     }
     
-    func centralManager(_ central: CBCentralManager, willRestoreState dict: [String : Any]) {
-        print("central: \(central)")
-        print("dict: \(dict)")
-    }
-    
 }
 
 extension ViewController: CBPeripheralDelegate {
-        
+    
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
-        guard let services = peripheral.services else { return }
+        if let error = error {
+            print("ERROR discovering services: \(error.localizedDescription)")
+            return
+        }
+        
+        guard let services = peripheral.services else {
+            print("No services found")
+            return
+        }
         
         for service in services {
-            print(service)
+//            print("Discovered service: \(service)")
             peripheral.discoverCharacteristics(nil, for: service)
         }
     }
@@ -133,46 +99,43 @@ extension ViewController: CBPeripheralDelegate {
             
             if characteristic.properties.contains(.read) {
                 peripheral.readValue(for: characteristic)
-                print("\(characteristic.uuid): properties contains .read")
+//                print("\(characteristic.uuid): properties contains .read")
             }
             
             if characteristic.properties.contains(.notify) {
                 
-                if characteristic.uuid == temperatureUUID {
+                if characteristic.uuid == temperatureAndHumidityUUID {
                     peripheral.setNotifyValue(true, for: characteristic)
                     peripheral.readValue(for: characteristic)
                 }
                 
-                if characteristic.uuid == humidityUUID {
-                    peripheral.setNotifyValue(true, for: characteristic)
-                    peripheral.readValue(for: characteristic)
-                    
-                }
-                                
-                print("\(characteristic.uuid): properties contains .notify")
+//                print("\(characteristic.uuid): properties contains .notify")
             }
             
         }
     }
     
-    func peripheral(_ peripheral: CBPeripheral, didUpdateNotificationStateFor characteristic: CBCharacteristic, error: Error?) {
-        if let error = error {
-            print("Error changing notification state: \(error.localizedDescription)")
-            return
-        }
-        
-        if characteristic.isNotifying {
-            print("Notifications enabled for characteristic: \(characteristic.uuid)")
-        } else {
-            print("Notifications disabled for characteristic: \(characteristic.uuid)")
-        }
-    }
+//    func peripheral(_ peripheral: CBPeripheral, didUpdateNotificationStateFor characteristic: CBCharacteristic, error: Error?) {
+//        if let error = error {
+//            print("Error changing notification state: \(error.localizedDescription)")
+//            return
+//        }
+//
+//        if characteristic.isNotifying {
+//            print("Notifications enabled for characteristic: \(characteristic.uuid)")
+//        } else {
+//            print("Notifications disabled for characteristic: \(characteristic.uuid)")
+//        }
+//    }
     
     func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
         switch characteristic.uuid {
             
-        case temperatureUUID:
+        case temperatureAndHumidityUUID:
             guard let value = characteristic.value else { return }
+            let byteArray = [UInt8](value)
+//            print(byteArray)  // [140, 10, 56, 43, 11] [온도, ?, 습도, ?, ?]
+                
             let temperatureData: Float = value.withUnsafeBytes { rawBufferPointer in
                 let buffer = rawBufferPointer.bindMemory(to: Int16.self).baseAddress!
                 let integerValue = Int16(littleEndian: buffer.pointee)
@@ -181,45 +144,26 @@ extension ViewController: CBPeripheralDelegate {
             }
             DispatchQueue.main.async {
                 self.temperatureLabel.text = "\(temperatureData)°C"
-                self.sendToServer(String(temperatureData))
-//                print("Temp: \(String(temperatureData))°C")
-            }
-            
-        case batteryLevelChracteristicCBUUID:
-            guard let characteristicData = characteristic.value, let byte = characteristicData.first else { return }
-            DispatchQueue.main.async {
-                self.batterLevelLabel.text = "\(String(byte))%"
-//                print("Battery: \(String(byte))%")
-            }
-            
-        case humidityUUID:
-            guard let characteristicData = characteristic.value else { return }
-            
-            if characteristicData.count >= 2 {
-                let humidityData = characteristicData.withUnsafeBytes { rawBufferPointer in
-                    let buffer = rawBufferPointer.bindMemory(to: Int16.self).baseAddress!
-                    return buffer.pointee
-                }
-                
-                let humidityPercentage = Float(humidityData)
-                DispatchQueue.main.async {
-                    self.humidityLabel.text = "\(humidityPercentage)%"
+                self.humidityLabel.text = "\(byteArray[2])%"
+                self.sendToServer(String(temperatureData), String(byteArray[2]))
+                if temperatureData > 24.7 {
+                    self.playSiren()
                 }
             }
-            
+        
         default:
             print("Unhandled Characteristic UUID: \(characteristic.uuid)")
         }
         
     }
     
-    func sendToServer(_ temp: String) {
+    func sendToServer(_ temp: String, _ humi: String) {
         guard let url = URL(string: "https://wkwebview.run.goorm.site/backgroundPush.php") else {
             print("Invaild URL")
             return
         }
         
-        let parameters = ["value": temp]
+        let parameters = ["temperature": temp, "humidity": humi]
         
         guard let jsonData = try? JSONSerialization.data(withJSONObject: parameters) else {
             print("Failed to serialize data")
@@ -244,7 +188,30 @@ extension ViewController: CBPeripheralDelegate {
             }
         }
         task.resume()
-
     }
     
+    func playSiren() {
+        
+        do {
+            try audioSession.setCategory(.playback, mode: .default, options: .mixWithOthers)
+            try audioSession.setActive(true)
+        } catch {
+            print("Failed to configure audio session: \(error.localizedDescription)")
+        }
+        
+        guard let path = Bundle.main.path(forResource: "siren", ofType: "mp3") else {
+            print("Failed to find the siren.mp3 file")
+            return
+        }
+        
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.prepareToPlay()
+            audioPlayer?.play()
+        } catch {
+            print("Failed to play the siren sound: \(error.localizedDescription)")
+        }
+    }
 }
